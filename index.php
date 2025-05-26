eee2ff5b1f106f854942880e3ecbe48dfaafc795
Peter Zerbinos <peter.zerbinos@mths.ca>
2025-05-26 17:11:46 +0000

    added answer.php

--- index.php
+++ index.php
@@ -26,52 +26,16 @@
       </div>
     </header>
     <main class="mdl-layout__content">
-      <div class="page-content">Click the button to get the current weather.
+      <div class="page-content">
+        <h4>Get the Current Weather for Ottawa</h4>
+        <p>Click the button below to retrieve the current weather conditions for Ottawa, Ontario.</p>
         <br />
-        <br />
-        <form action="" method="get">
+        <form action="answer.php" method="get">
           <button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent"
             type="submit" name="get_weather">
             Get Weather
           </button>
         </form>
-
-        <?php
-        if (isset($_GET['get_weather'])) {
-          $apiKey = "fe1d80e1e103cff8c6afd190cad23fa5"; // Your OpenWeatherMap API key
-          $latitude = "45.4211435";
-          $longitude = "-75.6900574";
-          $apiUrl = "https://api.openweathermap.org/data/2.5/weather?lat={$latitude}&lon={$longitude}&appid={$apiKey}";
-
-          // Use file_get_contents to make the API request
-          $jsonResult = @file_get_contents($apiUrl);
-
-          if ($jsonResult === FALSE) {
-            echo "<p>Sorry, an error has occurred. Please try again later. (API call failed)</p>";
-          } else {
-            $jsonData = json_decode($jsonResult);
-
-            if ($jsonData === NULL) {
-              echo "<p>Sorry, an error has occurred. Please try again later. (Invalid JSON response)</p>";
-            } else {
-              if (isset($jsonData->weather[0]) && isset($jsonData->main->temp)) {
-                $weatherDescription = $jsonData->weather[0]->description;
-                $weatherIconId = $jsonData->weather[0]->icon;
-                $weatherIconUrl = "https://openweathermap.org/img/wn/" . $weatherIconId . "@2x.png";
-                $currentWeatherKelvin = $jsonData->main->temp;
-                $currentWeatherCelsius = $currentWeatherKelvin - 273.15;
-
-                // Output
-                echo "<p> The current temperature is " . round($currentWeatherCelsius) . "°C. </p>";
-                echo "<p> The current weather is " . $weatherDescription . ". </p>";
-                echo "<img src='" . $weatherIconUrl . "' alt='Weather Icon'>";
-              } else {
-                echo "<p>Sorry, an error has occurred. Please try again later. (Missing weather data)</p>";
-              }
-            }
-          }
-        }
-        ?>
       </div>
     </main>
   </div>
